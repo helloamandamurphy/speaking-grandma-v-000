869e79e3e0d6da50de5cbee00437af9c5a389928
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-27 14:03:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,12 @@
-
+def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
+  if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end 
 
 def speak_to_grandma(phrase)
-  if !phrase.upcase
-    "NO, NOT SINCE 1938!"
-  end
-end
+  
